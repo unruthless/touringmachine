f8ef7dcfa142320b518ab35d3c4a681fa4f3333f
Aurelio Tinio <aurelio@tinio.org>
2012-06-11 11:27:18 -0700

    adds sample api endpoint /api/v0

---
+++ api.py
@@ -0,0 +1,14 @@
+"""
+API Endpoint for Touring Machine
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/api/v0')
+def home():
+    return "Hello, API World!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
