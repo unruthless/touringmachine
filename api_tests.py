b0bb3004542224d8f5c6ec423f107bbeb841d3c3
Aurelio Tinio <aurelio@tinio.org>
2012-06-29 13:33:25 -0700

    test api endpoint for required paramaters

--- api_tests.py
+++ api_tests.py
@@ -17,8 +17,15 @@ class APITestCase(unittest.TestCase):
         #os.unlink(api.app.config['DATABASE'])
         pass
 
-    def test_api_endpoint(self):
+    def test_endpoint(self):
         resp = self.api.get('/api/v0')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_200_when_required_parameters_provided(self):
+        resp = self.api.post('/api/v0', data=dict(
+            origin=94103,
+            destination=90210
+        ), follow_redirects=True)
         self.assertEqual(resp.status_code, 200)
 
 if __name__ == '__main__':
