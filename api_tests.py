ac7f7bdfaf987f18e0f78afb034b32e17bfdbb77
Aurelio Tinio <aurelio@tinio.org>
2012-06-29 13:03:24 -0700

    test api endpoint

--- api_tests.py
+++ api_tests.py
@@ -7,14 +7,19 @@ import api
 class APITestCase(unittest.TestCase):
 
     def setUp(self):
-        self.db_fd, api.app.config['DATABASE'] = tempfile.mkstemp()
-        api.app.config['TESTING'] = True
-        self.app = api.app.test_client()
-        app.init_db()
+        #self.db_fd, api.app.config['DATABASE'] = tempfile.mkstemp()
+        #api.app.config['TESTING'] = True
+        self.api = api.app.test_client()
+        #api.init_db()
 
     def tearDown(self):
-        os.close(self.db_fd)
-        os.unlink(api.app.config['DATABASE'])
+        #os.close(self.db_fd)
+        #os.unlink(api.app.config['DATABASE'])
+        pass
+
+    def test_api_endpoint(self):
+        resp = self.api.get('/api/v0')
+        self.assertEqual(resp.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
