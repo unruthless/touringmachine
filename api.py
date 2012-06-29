bdc7a72bb0a22d3d2542ae428034e286860685d8
Aurelio Tinio <aurelio@tinio.org>
2012-06-29 13:07:18 -0700

    change output on api GET

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/api/v0')
 def api():
-    return "Hello, API World!"
+    return "This is the touring machine api. Please make a post with a origin & destination parameters for output."
 
 
 if __name__ == '__main__':
