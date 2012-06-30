2fe19fc3354260e4f32ece6816b78fc06f0dda92
Aurelio Tinio <aurelio@tinio.org>
2012-06-29 17:37:23 -0700

    mock data directly from google directions api

--- api.py
+++ api.py
@@ -2,6 +2,8 @@
 API Endpoint for Touring Machine
 """
 
+import requests
+
 from flask import Flask, request
 app = Flask(__name__)
 
@@ -10,7 +12,13 @@ app = Flask(__name__)
 def api():
     if request.method == 'POST':
         if request.form['origin'] and request.form['destination']:
-            return "{'DUMMY': 'DATA'}"
+            params = {
+                'origin': request.form['origin'],
+                'destination': request.form['destination'],
+                'sensor': 'false'
+            }
+            r = requests.get('http://maps.googleapis.com/maps/api/directions/json', params=params)
+            return r.text
     else:
         return "This is the touring machine api.\
             Please POST with origin & destination parameters for valid output."
